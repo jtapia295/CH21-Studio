b35850942d92e7e7764a319a0be93e691d408d1d
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-07-08 12:39:40 -0400

    Commenced work on Studio for CH21

---
+++ CH21-Studio/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH21_Studio
+{
+    class Question
+    {
+        public string ActualQuestion { get; set; }
+
+        public List<string> Answers { get; set; }
+
+
+
+        public Question(string question, List<string> answers)
+        {
+            ActualQuestion = question;
+            Answers = answers;
+        }
+    }
+}
---
+++ CH21-Studio/Quiz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH21_Studio
+{
+    class Quiz : Question
+    {
+        public Dictionary<string, List<string>> AnswerKey { get; set; }
+
+        public void AddQuestion(string question, List<string> answers)
+        {
+
+        }
+
+        public Quiz()
+        {
+
+        }
+
+    }
+}
